7b20c12cea218ad547d372822f8ebafe4e20f8d7
Nick Kyimas <nkuimas@gmail.com>
2019-09-09 22:37:55 +0300

    r1.1.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,49 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		sass: {
-			dist: {
-				files: {
-					'css/main.css' : 'scss/styles.scss'
-				}
-			}
-		},
-		cssmin: {
-			dist: {
-				files: {
-					'css/main.min.css': 'css/main.css'
-				}
-			}
-		},
-		watch: {
-			css: {
-				files: '**/*.scss',
-				tasks: ['sass','cssmin'],
-                options: {
-                    livereload: true,
-                },
-			},
-            html: {
-                files: ['**/*.html'],
-                options: {
-                    livereload: true,
-                }
-            }
-		}
-	});
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default',['watch']);
-}
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    sass: {
+      dist: {
+        files: {
+          "css/main.css": "scss/styles.scss"
+        }
+      }
+    },
+    cssmin: {
+      dist: {
+        files: {
+          "css/main.min.css": "css/main.css"
+        }
+      }
+    },
+    uglify: {
+      options: {
+        mangle: false
+      },
+      my_target: {
+        files: {
+          "js/scripts.all.min.js": ["js/plugins.js", "js/main.js"]
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: "**/*.scss",
+        tasks: ["sass", "cssmin"],
+        options: {
+          livereload: true
+        }
+      },
+      html: {
+        files: ["**/*.html"],
+        options: {
+          livereload: true
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks("grunt-contrib-sass");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.registerTask("default", ["watch"]);
+};
